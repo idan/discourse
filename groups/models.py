6add41fd3a05d02081fc8bcff5fc7b73be4f5e4e
Meir Kriheli <meir@mksoft.co.il>
2011-02-03 00:26:59 +0200

    Add description, proper i18n for fields and model

--- groups/models.py
+++ groups/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class InterestGroup(models.Model):
-    name = models.CharField(max_length=70, blank=False)
-    public = models.BooleanField(default=True)
-    slug = models.SlugField(max_length=70, blank=True)
+    """The group holding the events"""
 
+    name        = models.CharField(_('Name'), max_length=70, unique=True)
+    public      = models.BooleanField(_('Public'), default=True)
+    slug        = models.SlugField(_('Slug'), max_length=70, blank=True,
+                    unique=True)
+    description = models.TextField(_('Description'), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('Interest group')
+        verbose_name_plural = _('Interest groups')
