cfc99ed0567c46f5c4baf91e3f14b6bdfc22ea06
Meir Kriheli <meir@mksoft.co.il>
2011-02-10 13:29:42 +0200

    Added Event and Talk models

--- groups/models.py
+++ groups/models.py
@@ -37,3 +37,43 @@ class Membership(models.Model):
     class Meta:
         verbose_name = _('Membership')
         verbose_name_plural = _('Memberships')
+
+
+class Event(models.Model):
+    """An event by an InterestGroup"""
+
+    group        = models.ForeignKey(InterestGroup, related_name='events',
+                     verbose_name=_('Interest group'))
+    start        = models.DateTimeField(_('Event start date and time'))
+    end          = models.DateTimeField(_('Event end'), blank=True, null=True)
+    description  = models.TextField(_('Description'), blank=True, null=True)
+    slug         = models.SlugField(_('Slug'), max_length=50, unique=True,
+                     help_text=_("Joined with group's slug forming the event's email address"))
+    published    = models.BooleanField(_('Published'), default=False)
+    location     = models.TextField(_('Location'), blank=True, null=True)
+    registration = models.BooleanField(_('Registration required'), default=False)
+    price        = models.DecimalField(_('Price'), max_digits=10,
+                    decimal_places=2, blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('Event')
+        verbose_name_plural = _('Events')
+
+    def __unicode__(self):
+        """Show as event_slug.group_slug, e.g: ev17.pywebil"""
+
+        return u'%(slug)s.%(group_slug)s' % {'group_slug':self.group.slug,
+                'slug': self.slug }
+
+class Talk(models.Model):
+    """Talks for an event"""
+
+    event   = models.ForeignKey(Event, verbose_name=_('Event'), related_name='talks')
+    title   = models.CharField(_('Talk title'), max_length=100)
+    content = models.TextField(_('Content'), blank=True, null=True)
+    start   = models.TimeField(_('Start time'), blank=True, null=True)
+    end     = models.TimeField(_('End time'), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('Talk')
+        verbose_name_plural = _('Talks')
